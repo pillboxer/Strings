ac949b7a22c3e21e8d476df05123fff8ede7d5e4
Henry Cooper <henrycooper@outlook.com>
2020-03-03 15:02:29 -0500

    Initial Commit

---
+++ Strings/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Strings
+//
+//  Created by Henry Cooper on 03/03/2020.
+//  Copyright © 2020 Henry Cooper. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
